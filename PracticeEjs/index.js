88e50c43ca81fec37dc1dd4b12785e71f08736cf
abduaslam <abduaslam098@gmail.com>
2026-01-26 11:22:03 +0200

    practice ejs

--- PracticeEjs/index.js
+++ PracticeEjs/index.js
@@ -1,10 +1,16 @@
-const express=require("express")
-const app=express();
-const PORT=4002;
-const path=require("path")
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"src","index.html"))
-})
-app.listen(PORT,()=>{
-    console.log(`the server is runing in port ${PORT}`)
-})
+const express = require("express");
+const path = require("path");
+const router = require("./src/router");
+
+const app = express();
+const PORT = 4002;
+
+app.use("/rout1", router);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
